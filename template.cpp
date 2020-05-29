c76d08218a648c1d454565ebc9f073147c643655
Ginto-A <hjt1273586928@163.com>
2020-05-29 10:47:46 +0800

    macro definition

--- template.cpp
+++ template.cpp
@@ -1,20 +1,9 @@
 #include<iostream>
 
-float Max(float x, float y){
-	std::cout << "float" << std::endl;
-	return x > y ? x : y;
-}
-int Max(int x, int y){
-	std::cout << "int" << std::endl;
-	return x > y ? x : y;
-}
-double Max(double x, double y){
-	std::cout << "double" << std::endl;
-	return x > y ? x : y;
-}
+#define Max(x, y) (x > y ? x : y)
 
 int main() {
-	std::cout << Max(9.9, 8.8) << std::endl;
+	std::cout << Max(9 + 0.9, 8 + 0.8) << std::endl;
 	std::cout << Max(9, 8) << std::endl;
 	return 0;
 }
